1c69bb25588423153fb33a54328bcdc076f7aea1
Vivek <itspersonal4vivek@gmai.com>
2025-11-11 18:41:54 +0530

    added debugging

--- server.js
+++ server.js
@@ -19,5 +19,6 @@ app.use('/api/budget', budgetRoutes);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log('Connected to MongoDB');
   })
   .catch((error) => console.log(error));
